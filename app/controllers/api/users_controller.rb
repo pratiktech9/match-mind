dfbb87e2444b80dd9a073f6d99157e265ab97dd9
nishanth.mosam <nishanth.mosam@instructure.com>
2025-09-27 16:35:22 +0530

    Rubocop fixes

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,7 +2,7 @@ class Api::UsersController < ApplicationController
   def show
     if session[:user_id]
       user = User.find(session[:user_id])
-      render json: { user: user.as_json(only: [:id, :name, :email, :image_url, :role]) }
+      render json: { user: user.as_json(only: [ :id, :name, :email, :image_url, :role ]) }
     else
       render json: { user: nil }
     end
