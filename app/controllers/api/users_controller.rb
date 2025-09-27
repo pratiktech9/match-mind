6f06feb58679111d4e95ca31c651b88b786549cd
nishanth.mosam <nishanth.mosam@instructure.com>
2025-09-27 16:33:45 +0530

    Adding omniauth support

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,10 @@
+class Api::UsersController < ApplicationController
+  def show
+    if session[:user_id]
+      user = User.find(session[:user_id])
+      render json: { user: user.as_json(only: [:id, :name, :email, :image_url, :role]) }
+    else
+      render json: { user: nil }
+    end
+  end
+end
