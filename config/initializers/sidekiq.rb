967b9b670f4760b3a0448126cfd4d053d6dc7b6a
pratik.bhagwat <pratik.bhagwat@instructure.com>
2025-09-28 11:09:54 +0530

    Fixes ci errors

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,5 @@
 require 'sidekiq/web'
-require 'sidekiq-cron/web'
+require 'sidekiq/cron/web'
 
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/0') }
